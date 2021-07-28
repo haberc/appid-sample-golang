1b3dcb42a0487a0d22e857abdb0635f5e47722c4
Christian Habermann <christian.habermann@de.ibm.com>
2021-07-28 21:12:37 +0200

    adds middleware function

--- main.go
+++ main.go
@@ -2,46 +2,48 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
-	"github.com/tkanos/gonfig"
-	"golang.org/x/oauth2"
+	"html/template"
 	"log"
 	"net/http"
-	"html/template"
 	"os"
 	"path"
 	"runtime"
 	"strings"
 	"time"
+
+	"github.com/tkanos/gonfig"
 	"golang.org/x/net/context"
-	"errors"
+	"golang.org/x/oauth2"
 )
 
 var conf oauth2.Config
+
 const APP_ID_CONFIG = "/config/appid_config.json"
 const OPEN_ID_SCOPE = "openid"
 const PROFILE_SCOPE = "profile"
 const STATE = "state"
-const SESSION_TOKEN ="session_token"
+const SESSION_TOKEN = "session_token"
 
 // Home struct, used for home.html template
-type Home struct{
+type Home struct {
 	Title string
-	User User
+	User  User
 }
 
 // User struct, holds all the user info shown in home.html
 type User struct {
-	Token string
+	Token   string
 	Profile string
 }
 
 // App ID configuration struct
 type AppIdConfiguration struct {
-	ClientId string
+	ClientId     string
 	ClientSecret string
-	AuthURL string
-	RedirectUrl string
+	AuthURL      string
+	RedirectUrl  string
 }
 
 // Builds a configuration object, with a given appidConfiguration struct
@@ -50,21 +52,20 @@ func buildConfigurationObject(app_id_configuration AppIdConfiguration) oauth2.Co
 	log.Println("Building configuration file.")
 
 	conf := &oauth2.Config{
-		ClientID: app_id_configuration.ClientId,
+		ClientID:     app_id_configuration.ClientId,
 		ClientSecret: app_id_configuration.ClientSecret,
-		RedirectURL: app_id_configuration.RedirectUrl,
-		Scopes: []string{OPEN_ID_SCOPE, PROFILE_SCOPE},
-		Endpoint:oauth2.Endpoint{
-			AuthURL: app_id_configuration.AuthURL+"/authorization",
-			TokenURL:  app_id_configuration.AuthURL+"/token",
-
+		RedirectURL:  app_id_configuration.RedirectUrl,
+		Scopes:       []string{OPEN_ID_SCOPE, PROFILE_SCOPE},
+		Endpoint: oauth2.Endpoint{
+			AuthURL:  app_id_configuration.AuthURL + "/authorization",
+			TokenURL: app_id_configuration.AuthURL + "/token",
 		},
 	}
 	return *conf
 }
 
 // Loads a configuration file, found in /config/appid_config.json
-func loadConfigurationFile() (AppIdConfiguration, error){
+func loadConfigurationFile() (AppIdConfiguration, error) {
 
 	log.Println("Loading configuration file.")
 
@@ -73,7 +74,7 @@ func loadConfigurationFile() (AppIdConfiguration, error){
 	// Using runtime.Caller, to make sure we get the path where the program is being executed
 	_, filename, _, ok := runtime.Caller(0)
 
-	if !ok{
+	if !ok {
 		return app_id_configuration, errors.New("Error calling runtime caller.")
 	}
 
@@ -88,13 +89,13 @@ func loadConfigurationFile() (AppIdConfiguration, error){
 }
 
 // Requests an OAuthToken using a "code" type
-func GetOauthToken(r *http.Request) (*oauth2.Token, error){
+func GetOauthToken(r *http.Request) (*oauth2.Token, error) {
 
 	log.Println("Getting auth token.")
 
 	ctx := context.Background()
 
-	if ctx == nil{
+	if ctx == nil {
 		return nil, errors.New("Could not get context.")
 	}
 
@@ -105,20 +106,20 @@ func GetOauthToken(r *http.Request) (*oauth2.Token, error){
 	// Exchange code for OAuth token
 	oauth2Token, oauth2TokenError := conf.Exchange(ctx, r.URL.Query().Get("code"))
 	if oauth2TokenError != nil {
-		return nil, errors.New("Failed to exchange token:"+ oauth2TokenError.Error())
+		return nil, errors.New("Failed to exchange token:" + oauth2TokenError.Error())
 	}
 
 	return oauth2Token, nil
 }
 
 // Requests a user profile, using a bearer token
-func GetUserProfile(r *http.Request, token oauth2.Token) (interface{}, error){
+func GetUserProfile(r *http.Request, token oauth2.Token) (interface{}, error) {
 
 	log.Println("Getting user profile.")
 
 	ctx := context.Background()
 
-	if ctx == nil{
+	if ctx == nil {
 		return nil, errors.New("Could not get context.")
 	}
 
@@ -126,9 +127,9 @@ func GetUserProfile(r *http.Request, token oauth2.Token) (interface{}, error){
 	client := conf.Client(ctx, &token)
 
 	// Get request using /userinfo url
-	userinfoResponse, userinfoError := client.Get(strings.Replace(conf.Endpoint.AuthURL,"/authorization","/userinfo",1))
+	userinfoResponse, userinfoError := client.Get(strings.Replace(conf.Endpoint.AuthURL, "/authorization", "/userinfo", 1))
 	if userinfoError != nil {
-		return nil, errors.New("Failed to obtain userinfo:"+userinfoError.Error())
+		return nil, errors.New("Failed to obtain userinfo:" + userinfoError.Error())
 	}
 
 	defer userinfoResponse.Body.Close()
@@ -149,29 +150,18 @@ func home(w http.ResponseWriter, r *http.Request) {
 	log.Println("Executing /home")
 
 	// Parssing home.html template
-	tmpl,_ := template.ParseFiles("./static/home.html")
+	tmpl, _ := template.ParseFiles("./static/home.html")
 	data := &Home{}
 
 	// Adding title to page
 	data.Title = "Welcome to AppID"
 
-	// Getting cookie named SESSION_TOKEN
-	cookie, err := r.Cookie(SESSION_TOKEN)
-
-	if err != nil{
-
-		// If no cookie found, that's ok, that means no user is logged in
-		log.Println("No session cookie found:"+err.Error())
-
-	}else {
+	if r.Context().Value("err") == nil {
 
 		log.Println("Session cookie found.")
 
-		// A cookie was found, this means a user is logged in
-		// Let's get the auth token value
-
 		authToken := oauth2.Token{
-			AccessToken: cookie.Value,
+			AccessToken: r.Context().Value("authToken").(string),
 		}
 
 		// Getting the user profile for the given auth token
@@ -211,11 +201,11 @@ func callback(w http.ResponseWriter, r *http.Request) {
 	// Getting auth token from request
 	authToken, error := GetOauthToken(r)
 
-	if error != nil{
+	if error != nil {
 
 		log.Println("Error getting auth token.")
 
-	}else {
+	} else {
 
 		log.Println("Setting session cookie.")
 
@@ -231,7 +221,7 @@ func callback(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// Redirecting to /home, in order to show the logged in user values
-	http.Redirect(w, r, "/home",http.StatusSeeOther)
+	http.Redirect(w, r, "/home", http.StatusSeeOther)
 
 }
 
@@ -243,11 +233,11 @@ func logout(w http.ResponseWriter, r *http.Request) {
 	// Getting session cookie
 	cookie, err := r.Cookie(SESSION_TOKEN)
 
-	if err != nil{
+	if err != nil {
 
-		log.Println("No session cookie found:"+err.Error())
+		log.Println("No session cookie found:" + err.Error())
 
-	}else{
+	} else {
 
 		log.Println("Session cookie found, invalidating it.")
 
@@ -263,13 +253,45 @@ func logout(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/home", http.StatusSeeOther)
 }
 
+func middleware(next http.Handler) http.Handler {
+
+	log.Println("Executing middleware")
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		//Do stuff
+
+		// Getting cookie named SESSION_TOKEN
+		cookie, err := r.Cookie(SESSION_TOKEN)
+
+		ctx := r.Context()
+
+		if err != nil {
+
+			// If no cookie found, that's ok, that means no user is logged in
+			log.Println("No session cookie found:" + err.Error())
+			ctx = context.WithValue(ctx, "err", err)
+
+		} else {
+
+			log.Println("Session cookie found.")
+
+			// A cookie was found, this means a user is logged in
+			// Let's get the auth token value
+			ctx = context.WithValue(ctx, "authToken", cookie.Value)
+		}
+
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+
+}
+
 func main() {
 
 	log.Println("Starting appid execution.")
 	// Loading App Id configuration file
-	app_id_configuration,  app_id_configuration_error := loadConfigurationFile()
+	app_id_configuration, app_id_configuration_error := loadConfigurationFile()
 
-	if app_id_configuration_error != nil{
+	if app_id_configuration_error != nil {
 
 		log.Println("Could not load configuration file.")
 
@@ -283,9 +305,9 @@ func main() {
 
 	// Creating handlers: /static /home /login /auth/callback /logout
 
-	http.Handle("/static/", http.StripPrefix("/static/",fs))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
-	http.HandleFunc("/home", home)
+	http.Handle("/home", middleware(http.HandlerFunc(home)))
 
 	http.HandleFunc("/login", login)
 
@@ -301,4 +323,3 @@ func main() {
 	http.ListenAndServe(port, nil)
 
 }
-
